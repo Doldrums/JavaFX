1fccc3f4e33ec807a0e505dab4c2045aaa68575a
iakhTim <otim142@ya.ru>
2019-05-17 18:50:28 +0500

    what
    what should i write here?

---
+++ src/Hell.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Hell {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Your name, please");
+        String name = sc.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+}
